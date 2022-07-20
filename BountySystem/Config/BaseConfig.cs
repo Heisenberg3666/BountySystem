d7456607ceda3c7ac33ce00f9a1f480c81453776
Dylan Morris <dylanmorris330@gmail.com>
2022-07-20 01:43:53 +0100

    Add project files.

---
+++ BountySystem/Config/BaseConfig.cs
@@ -0,0 +1,11 @@
+﻿using Exiled.API.Interfaces;
+
+namespace BountySystem.Config
+{
+    public class BaseConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public MessageConfig BountyMessages { get; set; } = new MessageConfig();
+    }
+}
---
+++ BountySystem/Events/ServerEvents.cs
@@ -0,0 +1,29 @@
+﻿using BountySystem.API;
+using BountySystem.API.Entities;
+using Exiled.Events.EventArgs;
+using Exiled.Events.Handlers;
+
+namespace BountySystem.Events
+{
+    internal class ServerEvents
+    {
+        public void RegisterEvents()
+        {
+            Server.EndingRound += OnRoundEnded;
+        }
+
+        public void UnregisterEvents()
+        {
+            Server.EndingRound -= OnRoundEnded;
+        }
+
+        private void OnRoundEnded(EndingRoundEventArgs e)
+        {
+            if (BountySystemApi.Bounties.Count > 0)
+            {
+                foreach (Bounty bounty in BountySystemApi.Bounties)
+                    BountySystemApi.FailBounty(bounty);
+            }
+        }
+    }
+}
